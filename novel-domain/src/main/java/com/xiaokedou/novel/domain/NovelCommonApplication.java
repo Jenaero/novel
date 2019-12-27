0e92ffd2ff6cc7aeef01013b2ce763880fc2e345
renyajian <renyajian@yf-fresh.com>
2019-12-27 16:32:33 +0800

    novel -> 移除不执行jar包maven依赖

---
+++ novel-domain/src/main/java/com/xiaokedou/novel/domain/NovelCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xiaokedou.novel.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelDomainApplication.class, args);
+	}
+
+}
