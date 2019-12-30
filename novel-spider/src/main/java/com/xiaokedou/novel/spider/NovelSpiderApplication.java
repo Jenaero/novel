cf86eb0633dea8e0d2a40f55fb043a7a28bae20a
renyajian <renyajian@yf-fresh.com>
2019-12-30 14:24:08 +0800

    novel -> 移除不执行jar包maven依赖

--- novel-spider/src/main/java/com/xiaokedou/novel/spider/NovelSpiderApplication.java
+++ novel-spider/src/main/java/com/xiaokedou/novel/spider/NovelSpiderApplication.java
@@ -5,18 +5,23 @@ import com.xiaokedou.novel.spider.storage.impl.TestNovelStorageImpl;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @ComponentScan("com.xiaokedou.novel")
 @MapperScan("com.xiaokedou.novel.dao.mapper")
-public class NovelSpiderApplication {
+public class NovelSpiderApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NovelSpiderApplication.class, args);
-		Processor processor = new TestNovelStorageImpl();
-		processor.process();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NovelSpiderApplication.class, args);
+        Processor processor = new TestNovelStorageImpl();
+        processor.process();
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(NovelSpiderApplication.class);
+    }
 }
