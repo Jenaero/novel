f37c28f880fa75353b38b3920bf8fb403c65edc4
renyajian <renyajian@yf-fresh.com>
2019-12-31 18:45:52 +0800

    novel -> 移除不执行jar包maven依赖

--- novel-web/src/main/java/com/xiaokedou/novel/web/controller/ShowNovelsController.java
+++ novel-web/src/main/java/com/xiaokedou/novel/web/controller/ShowNovelsController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/novel/")
 public class ShowNovelsController {
 	@Resource
 	private NovelService novelService;
